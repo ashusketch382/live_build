f6c129d08b009da8d1392e3dd5befe67ae03d51b
Ashutosh Singh <ashutoshsi@prod.hclpnp.com>
2025-01-25 14:02:52 +0530

    added cors npm package to skip cors error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { Client } = require("pg");
 
 const prisma = new PrismaClient();
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
