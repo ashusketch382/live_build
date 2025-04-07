dd5432a7da1649049edd8b64a7abd33cc5742d41
Ashutosh Singh <ashutoshsi@prod.hclpnp.com>
2025-04-07 11:13:08 +0530

    added date reset logic for starting update

--- index.js
+++ index.js
@@ -145,6 +145,9 @@ app.put('/build', async (req,res) => {
 app.put('/startUpdateBuilds', async (req,res) => {
     try {
         const data = {
+            buildStartTime: new Date("1970-01-01T00:00:00Z"),
+            buildEndTime: new Date("1970-01-01T00:00:00Z"),
+            buildStatus: "Build Started",
             onpremStatus: "Build Waiting",
             dockerStatus: "Build Waiting",
             comments: "Build Waiting"
